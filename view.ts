c30fed0dee4beb31f35fab4f17b180344b81006e
BT <anon@anon.anon>
2023-05-14 01:46:18 +0100

    Updated UI to have some config elements.

--- view.ts
+++ view.ts
@@ -22,24 +22,46 @@ export class CyberpunkView extends ItemView {
 		container.createEl("h4", {text: "Cyberpunk Assistant"});
 
 		// Network config
-		const configEl = this.contentEl.createDiv("cpunk-network-config");
+		const networkConfigEl = this.contentEl.createDiv("cpunk-network-config");
 
 			// Header
-		const networkHeaderEl = configEl.createDiv("cpunk-network-header");
+		const networkHeaderEl = networkConfigEl.createDiv("cpunk-network-header");
 		networkHeaderEl.createEl("h5", {text: "Network Generator"});
 
 			// Floors
-		const floorEl = configEl.createDiv("cpunk-network-floors");
-		floorEl.createSpan({text:"Floors: "})
-		const floorComponent = new TextComponent(floorEl);
+		networkConfigEl.createDiv({cls:"cpunk-option-header", text:"Number of Floors"});
+		const floorSpan = networkConfigEl.createSpan();
+		const rngRadio = floorSpan.createEl("input", {type: "radio", attr: {"id":"cpunk-random", "name":"floor_num_rng", "checked":"true"}})
+		floorSpan.createEl("label", {text: "Random (3d6)\n", attr:{"for":"cpunk-random"}, })
+		const definedRadio = floorSpan.createEl("input", {type: "radio", attr: {"id":"cpunk-defined", "name":"floor_num_rng"}})
+
+		const floorText = floorSpan.createEl("label")
+		const floorComponent = new TextComponent(floorText);
+		floorComponent.setDisabled(true);
+		// Doesn't look like you can limit the input via TextComponents :(
+
+
+		rngRadio.onClickEvent(function () {
+			floorComponent.setDisabled(true);
+		});
+		definedRadio.onClickEvent(function () {
+			floorComponent.setDisabled(false);
+		});
+
 
 			// Generate button
 		const netGenEl = this.contentEl.createDiv("cpunk-network-button");
 		const networkButton = netGenEl.createEl("button", {text: "Generate Network"})
 		networkButton.onClickEvent(function() {
-
 			testFunc(floorComponent.getValue());
 		});
+
+		// Character Generator
+		const characterEl = this.contentEl.createDiv("character-creator");
+
+			// Header
+		const characterHeaderEl = networkConfigEl.createDiv("cpunk-character-header");
+		characterHeaderEl.createEl("h5", {text: "Character Creator"});
 	}
 
 	async onClose() {
